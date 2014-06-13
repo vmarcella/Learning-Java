a4f5ff1069ee84c43877aa464b7e697d5d76cc12
Vincenzo Marcella <Epichdd@gmail.com>
2014-06-13 13:47:26 -0400

    Comment structure changed

--- src/Basic/Lesson07/IfStatements.java
+++ src/Basic/Lesson07/IfStatements.java
@@ -1,39 +1,33 @@
 package Basic.Lesson07;
 
-//Lesson on the commonly used, if statements. Used for decisions.
+/**
+ * <p>Demonstration on if statements.
+ * <p>"If" is a reserved word. Inside the parameters of the if(), goes the condition. The condition is tested to see
+ * if it's either true or false. Inside the brackets goes the statement, and if the statement is true, the code is executed. Otherwise
+ * , else's code is executed.
+ * 
+ * <p>The Testing operators are:
+ * <p><b> == : is Equal [if(x == 2)]
+ * <p><b> != : is not equal [if(x != 2)]
+ * <p><b> < : Less than [if(x < 12)]
+ * <p><b> > : Greater than [if(x > 12)]
+ * <p><b> >= : Greater than or equal to [if(x >= 32)]
+ * <p><b> <= : Less than or equal tp [if(x <= 45)]
+ * <p><b> %% : Has a remainder of [if(x %% 12)]
+ * 
+ * @author C3NZ
+ **/ 
 public class IfStatements {
 
 	public static void main(String[] args) {
 		
-		int test = 6;
+		int test = 6; //set test equal to 6
 		
-		//"if" is a reserved word. The () before the braces
-		//are also known as the Condition. That means
-		//its a test in other words to see if what you want is what
-		//you want.
-		//The Testing operators are:
-		//
-		// == Equal to
-		// != Not equal to
-		// < less than
-		// > greater than
-		// <= less than or equal to
-		// >= Greater than or equal to
-		// %% Has a remainder of
-		//
-		//After the condition, then goes the statement.
-		//the statement is placed inside braces after the condition.
-		//This is a basic true or false kind of way doing it.
-		//what it means is if 9 is equal to our integer test, then
-		//it will print out that it is, but if that condition is not true, then
-		//print out that it is not.
-		//Play around with it!
-		if (test == 9){
-			System.out.println("Test is equal to Nine.");
-		}else{
-			System.out.println("No it is not.");
+	
+		if (test == 9){ //is test equal to 9?
+			System.out.println("Test is equal to Nine."); //does not work
+		}else{ //if not true
+			System.out.println("No it is not."); //works by default
 		}
-		
 	}
-
 }
