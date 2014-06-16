50df27d4f3f7d7c8eb18b9d159d289602b6e3d63
Vincenzo Marcella <epichdd@gmail.com>
2014-06-15 21:40:58 -0400

    Jason's a bitch

--- src/Basic/Lesson08/LogicalOperators.java
+++ src/Basic/Lesson08/LogicalOperators.java
@@ -5,15 +5,15 @@ public class LogicalOperators {
 	
 	public static void main(String args[]){
 		
-		int boy, girl;
+		int firstNumber, secondNumber;
 		
-		boy = 18;
-		girl = 40;
+		firstNumber = 18;
+		secondNumber = 40;
 		
 		//little dating system to check if two people can date.
 		//&& means that things on each side have to be true.
 		// || is or. Means that one has to be true.
-		if(boy > 10 && girl < 60){
+		if(firstNumber > 10 && secondNumber < 60){
 			System.out.println("You can date!");
 		}else{
 			System.out.println("You cannot date!");
