3f010bb2b27b72eac537bc1cf3a99d58d8348315
Vincenzo Marcella <Epichdd@gmail.com>
2014-06-16 11:20:36 -0400

    Changed comment structure

--- src/Basic/Lesson08/LogicalOperators.java
+++ src/Basic/Lesson08/LogicalOperators.java
@@ -1,8 +1,10 @@
 package Basic.Lesson08;
 
 /**
- * Jason Schanils a bitch
- * 
+ * <p>Demonstrations on  multiple tests, or also known as Logical Operators.</p>
+ * <p>The Logical Operators are:</p>
+ * <p><b>&& means "and".Both conditions must be true in order to be true.[if conditon && condition are true / false]</p></b>
+ * <p><b>|| means "or". One condtiio n has to be true in order to make the whole statement true. [if condition || condition are true / false]
  * 
  * @author C3NZ
  *
@@ -13,16 +15,19 @@ public class LogicalOperators {
 		
 		int firstNumber, secondNumber;
 		
-		firstNumber = 18;
+		firstNumber = 11;
 		secondNumber = 40;
 		
-		//little dating system to check if two people can date.
-		//&& means that things on each side have to be true.
-		// || is or. Means that one has to be true.
-		if(firstNumber > 10 && secondNumber < 60){
-			System.out.println("You can date!");
+		if(firstNumber > 10 || secondNumber < 60){ //will be true by default
+			System.out.println("First statement is true");
 		}else{
-			System.out.println("You cannot date!");
+			System.out.println("FIrst statement was not true");
+		}
+		
+		if(firstNumber > 10 && firstNumber < 60){ //will be false
+			System.out.println("Second statement is true");
+		}else{
+			System.out.println("Second statement is false");
 		}
 	}
 }
