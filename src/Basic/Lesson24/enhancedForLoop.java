f850b3025083c53d1b2a72f5e75dbd24079514ad
Cenz <Epichdd@gmail.com>
2014-08-20 23:00:00 -0400

    Update

--- src/Basic/Lesson24/enhancedForLoop.java
+++ src/Basic/Lesson24/enhancedForLoop.java
@@ -8,13 +8,12 @@ public class enhancedForLoop {
 
 	public static void main(String[] args){
 		
-		int array[] = {2,5,6,7};
+	int array[] = {2,5,6,7};
 		int total = 0;
-		
-		//needs type of data in the identifier.
-		//the i is storing the value of the arrays
-		//of all indexes. then it takes the name of the
-		//array its collecting this data from.
+		//Variable created inside for loop must be same type as Array you're using
+		//the i is stores the value of the arrays indexes that is on the other side of the colon
+		//you can basically say int i iterates through each index of the array and everytime it goes through
+		//an index it stores itself as the value the index is holding
 		for(int i : array){
 			total += i;
 		}
